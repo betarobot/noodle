1ce66114ee263cf16ef934d406e69083efd2c32d
betarobot <andrey.pshenichny@gmail.com>
2010-09-26 22:43:48 +0200

    Changes to block H2 elements to be h2.title

--- noodle/base/block.tpl.php
+++ noodle/base/block.tpl.php
@@ -3,12 +3,12 @@
 <div id="block-<?php print $block->module .'-'. $block->delta; ?>" class="block block-<?php print $block->module ?>">
   
   <?php if ($block->region == "sidebar_accordion"): ?>
-    <h2><a href="#accordion-link-<?php print $block_id ?>" class="accordionlink"><?php print $block->subject ?></a></h2>
+    <h2 class="title"><a href="#accordion-link-<?php print $block_id ?>" class="accordionlink"><?php print $block->subject ?></a></h2>
     
   <?php else:?>
   
     <?php if ($block->subject): ?>
-      <h2><?php print $block->subject ?></h2>
+      <h2 class="title"><?php print $block->subject ?></h2>
     <?php endif;?>
   
   <?php endif;?>
